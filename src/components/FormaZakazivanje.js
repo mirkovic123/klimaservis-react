77c5d8f8d0e0c253bcd1a464de9320f6e3639781
mirkovic123 <mirkoviclj15@gmail.com>
2022-09-14 01:27:03 +0200

    Kontrola statea za polja forme, useState hooks

--- src/components/FormaZakazivanje.js
+++ src/components/FormaZakazivanje.js
@@ -1,6 +1,42 @@
+import { useState } from 'react'
 
 
 const FormaZakazivanje = () => {
+
+    const [servis, setServis] = useState({
+        ime: '',
+        prezime: '',
+        adresa: '',
+        grad: '',
+        tip: '',
+        datum: ''
+    });
+
+    const handleIme = (e) => {
+        setServis({ ...servis, ime: e.target.value })
+    }
+
+    const handlePrezime = (e) => {
+        setServis({ ...servis, prezime: e.target.value })
+    }
+
+    const handleAdresa = (e) => {
+        setServis({ ...servis, adresa: e.target.value })
+    }
+
+    const handleGrad = (e) => {
+        setServis({ ...servis, grad: e.target.value })
+    }
+
+    const handleTip = (e) => {
+        setServis({ ...servis, tip: e.target.value })
+    }
+
+    const handleDatum = (e) => {
+        setServis({ ...servis, datum: e.target.value })
+    }
+
+
 
     return (
         <div className="formazakazivanje-div">
@@ -9,27 +45,27 @@ const FormaZakazivanje = () => {
 
                 <div>
                     <span>Ime: </span>
-                    <input type="text" className="form-control mt-1 text-center" />
+                    <input type="text" className="form-control mt-1 text-center" value={servis.ime} onChange={handleIme} />
                 </div>
 
                 <div>
                     <span>Prezime: </span>
-                    <input type="text" className="form-control mt-1 text-center" />
+                    <input type="text" className="form-control mt-1 text-center" value={servis.prezime} onChange={handlePrezime} />
                 </div>
 
                 <div>
                     <span>Adresa: </span>
-                    <input type="text" className="form-control mt-1 text-center" />
+                    <input type="text" className="form-control mt-1 text-center" value={servis.adresa} onChange={handleAdresa} />
                 </div>
 
                 <div>
                     <span>Grad: </span>
-                    <input type="text" className="form-control mt-1 text-center" />
+                    <input type="text" className="form-control mt-1 text-center" value={servis.grad} onChange={handleGrad} />
                 </div>
 
                 <div>
                     <span>Tip: </span>
-                    <select type="text" className="form-select mt-1 text-center">
+                    <select type="text" className="form-select mt-1 text-center" value={servis.tip} onChange={handleTip}>
                         <option>BASIC</option>
                         <option>MEDIUM</option>
                         <option>PREMIUM</option>
@@ -38,11 +74,12 @@ const FormaZakazivanje = () => {
 
                 <div>
                     <span>Datum: </span>
-                    <input type="text" className="form-control mt-1" />
+                    <input type="text" className="form-control mt-1" value={servis.datum} onChange={handleDatum} />
                 </div>
 
             </form>
 
+            <button type="button" className="btn btn-dark" id="button-check">PROVERI TERMIN</button>
 
         </div>
     )
